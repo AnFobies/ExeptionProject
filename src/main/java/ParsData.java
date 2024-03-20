a5b66a5027810f2c5f3a04cf0d4fceb5a1291814
Anton <toshasuhanov@ya.ru>
2024-03-20 13:07:47 +0300

    update project

--- src/main/java/ParsData.java
+++ src/main/java/ParsData.java
@@ -6,18 +6,28 @@ public class ParsData {
         String[] data = inputData.enterData();
         HashMap<String, String> dataDict = new HashMap<>();
         StringBuilder sb = new StringBuilder();
-        for (String i: data){
-            if(i.length() == 1){
-                if (i.equals("f") || i.equals("m")){
+        for (String i : data) {
+            if (i.length() == 1) {
+                if (i.equals("f") || i.equals("m")) {
                     dataDict.put("sex", i);
                 } else {
                     try {
                         throw new SexException();
-                    } catch (SexException e){
+                    } catch (SexException e) {
                         e.sexException();
                     }
                 }
-            } else if (i.matches("\\d{1,2}\\.\\d{1,2}\\.\\d{4}")) {
+            }
+        }
+        if (!dataDict.containsKey("sex")){
+            try {
+                throw new SexException();
+            } catch (SexException e) {
+                e.sexException();
+            }
+        }
+        for (String i : data) {
+            if (i.matches("\\d{1,2}\\.\\d{1,2}\\.\\d{4}")) {
                 if(DateCheck.isDateValid(i)){
                     dataDict.put("date", i);
                 } else {
@@ -27,17 +37,30 @@ public class ParsData {
                         e.dateException();
                     }
                 }
-
-            } else if (i.matches("[0-9]+")){
-                dataDict.put("tel", i);
-            } else if (i.matches("[А-Яа-яA-Za-z]+")) {
-                sb.append(i).append(" ");
-            } else {
-                try {
-                    throw new DataException();
-                } catch (DataException e){
-                    e.dataException();
+            }
+        }
+        if (!dataDict.containsKey("date")){
+            try {
+                throw new DateException();
+            } catch (DateException e) {
+                e.dateException();
+            }
+        }
+        for (String i : data) {
+            if (i.matches("[0-9]+")) {
+                dataDict.put("phone", i);
                 }
+        }
+        if (!dataDict.containsKey("phone")){
+            try {
+                throw new PhoneException();
+            } catch (PhoneException e) {
+                e.phoneException();
+            }
+        }
+        for (String i : data) {
+            if (i.matches("[А-Яа-яA-Za-z]+") && i.length() > 1) {
+                sb.append(i).append(" ");
             }
         }
         String[] fullName = String.valueOf(sb).split(" ");
@@ -46,6 +69,14 @@ public class ParsData {
             dataDict.put("Имя", fullName[1]);
             dataDict.put("Отчество", fullName[2]);
         }
+        if (!dataDict.containsKey("Фамилия") && !dataDict.containsKey("Имя") && !dataDict.containsKey("Отчество")){
+            try {
+                throw new DataException();
+            } catch (DataException e){
+                e.dataException();
+            }
+        }
         return dataDict;
     }
 }
+
