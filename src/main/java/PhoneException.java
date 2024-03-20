801afcfedb6bcc283099e85ef4fc857d2d3ea260
Anton <toshasuhanov@ya.ru>
2024-03-20 13:08:03 +0300

    update project

---
+++ src/main/java/PhoneException.java
@@ -0,0 +1,9 @@
+public class PhoneException extends Exception{
+    public PhoneException() {
+
+    }
+
+    public void phoneException(){
+        System.out.println("Вы ввели некорректный номер телефона");
+    }
+}
